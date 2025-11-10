3bc9ab26fe1505615a3c19b219d2dd203bcdc7f3
Adi <adi.causevic@protonmail.com>
2025-11-10 22:51:02 +0100

    sandbox playing

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,35 @@ const std = @import("std");
 const rl = @import("raylib");
 
 pub fn main() anyerror!void {
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const image = try rl.Image.init("earring.png");
+    std.log.info("Image size: {d}x{d}", .{ image.width, image.height });
 
-    rl.initWindow(screenWidth, screenHeight, "Pale");
+    rl.initWindow(image.width, image.height, "Pale");
     defer rl.closeWindow();
-
     rl.setTargetFPS(60);
+
+    // const texture = try rl.Texture2D.init("earring.png");
+
+    var renderTexture = try rl.RenderTexture2D.init(image.width, image.height);
+    {
+        renderTexture.begin();
+        defer renderTexture.end();
+        rl.clearBackground(.blue);
+        rl.drawCircle(50, 50, 50, .green);
+    }
+    var textureImage = try rl.Image.fromTexture(renderTexture.texture);
+
+    for (0..500) |x| {
+        for (0..500) |y| {
+            textureImage.drawPixel(@intCast(x), @intCast(y), image.getColor(@intCast(x), @intCast(y)));
+        }
+    }
+
+    textureImage.drawCircle(200, 200, 20, .pink);
+
+    const finalTexture = try rl.Texture2D.fromImage(textureImage);
+
+    var buffer: [16]u8 = undefined;
 
     var counter: u8 = 0;
     while (!rl.windowShouldClose()) {
@@ -22,7 +44,9 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
+        rl.drawTexture(finalTexture, 0, 0, .white);
         rl.drawRectangle(100, 100, 100, 100, color);
-        rl.drawText("Raylib window", 190, 200, 20, .light_gray);
+        const text = try std.fmt.bufPrintZ(&buffer, "{}", .{counter});
+        rl.drawText(text, 190, 200, 40, .light_gray);
     }
 }
